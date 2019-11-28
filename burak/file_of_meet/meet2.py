2f875169b199508f748c396e0a3fef881aa84a68
0robert0 <56759095+0robert0@users.noreply.github.com>
2019-11-29 00:18:15 +0200

    2. derste kullanılan dosya eklendi

---
+++ burak/file_of_meet/meet2.py
@@ -0,0 +1,18 @@
+a,b,c,d =1,-2,13,45
+e=10
+f = int()
+print(a)
+print(g := 16)
+print(1,2,3, sep="_")
+print("Merhaba\tDünya")
+print(type(e))
+print(f)
+a="Mer"
+b="haba"
+c=a+b
+print(a,b)
+a=5.3
+a="5"
+c=a+str(d)
+print(c)
+a=[1,2,3,4,"asd","2fds"]
