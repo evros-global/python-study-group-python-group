a83eaecc3fe3077d730db2e063e5d2a85fbf6610
Mfo3566 <Mfo1997248@gmail.com>
2019-12-01 13:23:00 +0300

    gec oldu ama guc olmadi 3.odev
    gec oldu ama guc olmadi 3.odev

---
+++ Mfo/exercise 3.py
@@ -0,0 +1,25 @@
+a="c:/folder1/folder2/file.ext"
+
+
+
+
+
+
+print(b)
+print(c)
+print(d)
+
+#the output must be
+"""
+c:/folder1/folder2
+file
+ext
+"""
+
+
+b="c:/folder1/folder2"
+print(b)
+c="file"
+print(c)
+d="ext"
+print(d)
