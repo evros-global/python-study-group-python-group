f834926315e88bc00874e2ab7e476f7281ae4a8c
Mfo3566 <Mfo1997248@gmail.com>
2019-12-02 00:06:13 +0300

    homework1
    homework1

---
+++ Mfo/homework-1.py
@@ -0,0 +1,12 @@
+#this is a homework
+"""You should do 7 assignment each data type below.
+ - str - int - float
+then print them on screen."""
+int=('1')
+print(int)
+a='0.12'
+b='0.15'
+a+b
+mystring=("hello world")
+mystring
+print('mystring')
