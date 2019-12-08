0423165f32a85ae8b0f3e0e7410ee108da919f09
Hüseyinkc <56474366+huseyinkc@users.noreply.github.com>
2019-12-08 20:38:51 +0300

    Create öde4.py

---
+++ mark/öde4.py
@@ -0,0 +1,18 @@
+#print((25*68) + float(13/28))
+
+quilt_width= 8
+quilt_length= 12
+print(quilt_width * quilt_length)
+
+quilt_length= 8
+print(quilt_width * quilt_length)
+
+print(6 ** 2)
+print(7 ** 2)
+print(8 ** 2)
+
+print(6 ** 3)
+
+sayı= int(input("sayı"))
+for i in sayı:
+    if(sayı)
