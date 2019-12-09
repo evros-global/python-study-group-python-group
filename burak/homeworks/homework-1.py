fe02d5c611d84335d10d6e222861616e46aec318
Berk3541 <58194819+Berk3541@users.noreply.github.com>
2019-12-09 18:31:34 +0300

    Homework 1

--- burak/homeworks/homework-1.py
+++ burak/homeworks/homework-1.py
@@ -2,3 +2,21 @@
 """You should do 7 assignment each data type below.
  - str - int - float
 then print them on screen."""
+
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+
+
